23a511ffca41d703bfdc5476abd8e38e5527c350
agarbows <01187224@pw.edu.pl>
2024-06-03 11:31:46 +0200

    Added Field Class

--- MinesweeperLogicalLib/Field.cpp
+++ MinesweeperLogicalLib/Field.cpp
@@ -1,1 +1,52 @@
 #include "Field.h"
+#include <iostream>
+
+Field::Field() noexcept
+{
+	isMine = false;
+	isFlagged = false;
+	isRevealed = false;
+	adjacentMines = 0;
+}
+
+bool Field::IsMine() const noexcept
+{
+	return isMine;
+}
+
+void Field::SetMine() noexcept
+{
+	isMine = true;
+}
+
+bool Field::IsFlagged() const noexcept
+{
+	return isFlagged;
+}
+
+void Field::ToggleFlag() noexcept
+{
+	isFlagged = !isFlagged;
+}
+
+bool Field::IsRevealed() const noexcept
+{
+	return isRevealed;
+}
+
+void Field::Reveal() noexcept
+{
+	if (isRevealed = false) {
+		isRevealed = true;
+	}
+}
+
+int Field::getAdjacentMines() noexcept
+{
+	return adjacentMines;
+}
+
+void Field::setAdjacentMines(int mines) noexcept
+{
+	adjacentMines = mines;
+}
--- MinesweeperLogicalLib/Field.h
+++ MinesweeperLogicalLib/Field.h
@@ -1,5 +1,21 @@
 #pragma once
 class Field
 {
+	public:
+		Field() noexcept;
+		~Field() noexcept = default;
+		bool IsMine() const noexcept;
+		void SetMine() noexcept;
+		bool IsFlagged() const noexcept;
+		void ToggleFlag() noexcept;
+		bool IsRevealed() const noexcept;
+		void Reveal() noexcept;
+		int getAdjacentMines() noexcept;
+		void setAdjacentMines(int mines) noexcept;
+	private:
+		bool isMine;
+		bool isFlagged;
+		bool isRevealed;
+		int adjacentMines;
 };
 
